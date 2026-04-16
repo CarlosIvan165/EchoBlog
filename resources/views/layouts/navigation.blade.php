9152c4dbaa702f66f7dae030d2217680e5772432
CarlosIvan165 <139912913+CarlosIvan165@users.noreply.github.com>
2026-04-16 15:14:48 -0600

    modificacion
    eliminacion de icono

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,12 +37,6 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden lg:flex sm:items-center sm:ms-6">
-                <button @click="open = !open; "
-                        class="relative px-3 py-2 text-gray-700 hover:text-indigo-600 focus:outline-none">
-                    🔔
-                        <span class="absolute top-0 right-1 inline-flex items-center justify-center px-2 py-2 text-xs font-bold text-white bg-red-600 rounded-full">
-                        </span>
-                </button>
                 @guest
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 lg:flex">
                         <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
